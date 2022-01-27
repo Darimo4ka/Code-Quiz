522d9e25d4e8007b025274dfc109b89792dd8eb6
Darimo4ka <darima_badm@yahoo.com>
2022-01-27 15:19:37 -0500

    worked with displaying functions to get my from one question to other and console.log correct or incorret response form user

--- quize.js
+++ quize.js
@@ -1,43 +1,87 @@
 //var for DOM elements 
 var startBtn = document.querySelector(".start-button");
 var questionBox = document.querySelector(".question-box");
+var endBtn  = document.querySelector(".end-game");
 
-
-// create an array of objects for quiz quwestions and answers
+// create an array of objects for quiz questions and answers
 var questions = [
   {
-    question: "This question one?", 
-    answers: ["answer1", "answer2", "answer3", "answer4"],
-    correct: "answer3"
+    question: "What tag defines a division or the beginning/end of an individual section in an HTML document?", 
+    answers: ["table", "image", "div", "meta"],
+    correct: "div"
   },
   {
-    question: "This question two?", 
-    answers: ["answer1", "answer2", "answer3", "answer4"],
-    correct: "answer1"
+    question: "What tag is used to define a list item (in a bulleted list)?", 
+    answers: ["li", "ul", "s", "u"],
+    correct: "li"
   },
   {
-    question: "This question three?", 
-    answers: ["answer1", "answer2", "answer3", "answer4"],
-    correct: "answer4"
+    question: "What element is a container for all the head elements, and may include the document title, scripts, styles, meta information, and more?", 
+    answers: ["title", "footer", "body", "head"],
+    correct: "head"
   }
 ]
 // create varables to control the flow of the quiz
-var currentQuest = 0
+var currentQuestion = 0
 // write function to display questions
 function displayQuestion(){
   console.log("displaying")
+
+
+  // questions.textContent = questions[currentQuestion].question;
+  // questionAnswer = questions[currentQuestion].answers;
+
+  // for (let i = 0; i < questions.length; i++){
+  //   let answerBtnEl = document.createElement("button");
+  //   answerBtnEl.textContent = questions[i]
+    
+  // }
+
+
   // create template for HTMl inside question box
   var template = `
-  <h2>${questions[currentQuest].question}</h2>
-  <button> ${questions[currentQuest].answers[0]}</button>
-  <button> ${questions[currentQuest].answers[1]}</button>
-  <button> ${questions[currentQuest].answers[2]}</button>
-  <button> ${questions[currentQuest].answers[3]}</button>
+  <h2>${questions[currentQuestion].question}</h2>
+  <button onclick="evaluateQuestion(this)">${questions[currentQuestion].answers[0]}</button>
+  <button onclick="evaluateQuestion(this)">${questions[currentQuestion].answers[1]}</button>
+  <button onclick="evaluateQuestion(this)">${questions[currentQuestion].answers[2]}</button>
+  <button onclick="evaluateQuestion(this)">${questions[currentQuestion].answers[3]}</button>
  
   `
-  // figure out listen click
+
+
   // inject template into question box with inner html method
-  questionBox.innerHTML = template
+  questionBox.innerHTML = template;
+}
+
+
+function evaluateQuestion(event){
+console.log(event)//we want to see what we are clicking
+
+//if we click answer 1 event.target.textContent is equal to answer1
+console.log(event.textContent)
+//event.target.textContent is going to give us the text of the answer 
+var userChoice= event.textContent;
+//if answer1 is the same as questions[currentQuestion].correct
+var correctAnswer=questions[currentQuestion].correct;
+console.log(correctAnswer)
+
+if(userChoice===correctAnswer){
+  console.log('correct!')
+  //
+}else{
+  console.log('incorrect')
+  //decrease timer
+}
+
+if(currentQuestion < questions.length-1){
+  currentQuestion++
+  displayQuestion()
+}
+else{
+  // function to end game and clear highscores
+ 
+}
+
 }
 // write a function to start a game
 function startGame(){
@@ -53,215 +97,29 @@ startBtn.addEventListener("click", startGame);
 
 
 
+// // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+// function startTimer() {
+//   // Sets timer
+//   timer = setInterval(function() {
+//     timerCount--;
+//     timerElement.textContent = timerCount;
+//     if (timerCount >= 0) {
+//       // Tests if win condition is met
+//       if (isWin && timerCount > 0) {
+//         // Clears interval and stops timer
+//         clearInterval(timer);
+//         winGame();
+//       }
+//     }
 
-/* Higt score elements
-score screen element
-timer element
-quize element
-instraction part
-start screen
-function to check ansewers
-function to load next 
-function to input
-next btn will display quiz
-start btn
-*/
+// // Updates win count on screen and sets win count to client storage
+// function setWins() {
+//   win.textContent = winCounter;
+//   localStorage.setItem("winCount", winCounter);
+// }
 
-
-
-
-
-
-
-
-
-/*var wordBlank = document.querySelector(".word-blanks");
-var win = document.querySelector(".win");
-var lose = document.querySelector(".lose");
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
-
-var chosenWord = "";
-var numBlanks = 0;
-var winCounter = 0;
-var loseCounter = 0;
-var isWin = false;
-var timer;
-var timerCount;
-
-// Arrays used to create blanks and letters on screen
-var lettersInChosenWord = [];
-var blanksLetters = [];
-
-// Array of words the user will guess
-var words = ["variable","array", "modulus", "object", "function", "string", "boolean"];
-
-// The init function is called when the page loads 
-function init() {
-  getWins();
-  getlosses();
-}
-
-// The startGame function is called when the start button is clicked
-function startGame() {
-  isWin = false;
-  timerCount = 10;
-  // Prevents start button from being clicked when round is in progress
-  startButton.disabled = true;
-  renderBlanks()
-  startTimer()
-}
-
-// The winGame function is called when the win condition is met
-function winGame() {
-  wordBlank.textContent = "YOU WON!!!🏆 ";
-  winCounter++
-  startButton.disabled = false;
-  setWins()
-}
-
-// The loseGame function is called when timer reaches 0
-function loseGame() {
-  wordBlank.textContent = "GAME OVER";
-  loseCounter++
-  startButton.disabled = false;
-  setLosses()
-}
-
-// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-function startTimer() {
-  // Sets timer
-  timer = setInterval(function() {
-    timerCount--;
-    timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-        // Clears interval and stops timer
-        clearInterval(timer);
-        winGame();
-      }
-    }
-    // Tests if time has run out
-    if (timerCount === 0) {
-      // Clears interval
-      clearInterval(timer);
-      loseGame();
-    }
-  }, 1000);
-}
-
-// Creates blanks on screen
-function renderBlanks() {
-  // Randomly picks word from words array
-  chosenWord = words[Math.floor(Math.random() * words.length)];
-  lettersInChosenWord = chosenWord.split("");
-  numBlanks = lettersInChosenWord.length;
-  blanksLetters = []
-  // Uses loop to push blanks to blankLetters array
-  for (var i = 0; i < numBlanks; i++) {
-    blanksLetters.push("_");
-  }
-  // Converts blankLetters array into a string and renders it on the screen
-  wordBlank.textContent = blanksLetters.join(" ")
-}
-
-// Updates win count on screen and sets win count to client storage
-function setWins() {
-  win.textContent = winCounter;
-  localStorage.setItem("winCount", winCounter);
-}
-
-// Updates lose count on screen and sets lose count to client storage
-function setLosses() {
-  lose.textContent = loseCounter;
-  localStorage.setItem("loseCount", loseCounter);
-}
-
-// These functions are used by init
-function getWins() {
-  // Get stored value from client storage, if it exists
-  var storedWins = localStorage.getItem("winCount");
-  // If stored value doesn't exist, set counter to 0
-  if (storedWins === null) {
-    winCounter = 0;
-  } else {
-    // If a value is retrieved from client storage set the winCounter to that value
-    winCounter = storedWins;
-  }
-  //Render win count to page
-  win.textContent = winCounter;
-}
-
-function getlosses() {
-  var storedLosses = localStorage.getItem("loseCount");
-  if (storedLosses === null) {
-    loseCounter = 0;
-  } else {
-    loseCounter = storedLosses;
-  }
-  lose.textContent = loseCounter;
-}
-
-function checkWin() {
-  // If the word equals the blankLetters array when converted to string, set isWin to true
-  if (chosenWord === blanksLetters.join("")) {
-    // This value is used in the timer function to test if win condition is met
-    isWin = true;
-  }
-}
-
-// Tests if guessed letter is in word and renders it to the screen.
-function checkLetters(letter) {
-  var letterInWord = false;
-  for (var i = 0; i < numBlanks; i++) {
-    if (chosenWord[i] === letter) {
-      letterInWord = true;
-    }
-  }
-  if (letterInWord) {
-    for (var j = 0; j < numBlanks; j++) {
-      if (chosenWord[j] === letter) {
-        blanksLetters[j] = letter;
-      }
-    }
-    wordBlank.textContent = blanksLetters.join(" ");
-  }
-}
-
-  // Attach event listener to document to listen for key event
-document.addEventListener("keydown", function(event) {
-  // If the count is zero, exit function
-  if (timerCount === 0) {
-    return;
-  }
-  // Convert all keys to lower case
-  var key = event.key.toLowerCase();
-  var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
-  // Test if key pushed is letter
-  if (alphabetNumericCharacters.includes(key)) {
-    var letterGuessed = event.key;
-    checkLetters(letterGuessed)
-    checkWin();
-  }
-});
-
-// Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", startGame);
-
-// Calls init() so that it fires when page opened
-init();
-
-// Bonus: Add reset button
-var resetButton = document.querySelector(".reset-button");
-
-function resetGame() {
-  // Resets win and loss counts
-  winCounter = 0;
-  loseCounter = 0;
-  // Renders win and loss counts and sets them into client storage
-  setWins()
-  setLosses()
-}
-// Attaches event listener to button
-resetButton.addEventListener("click", resetGame*/
+// // Updates lose count on screen and sets lose count to client storage
+// function setLosses() {
+//   lose.textContent = loseCounter;
+//   localStorage.setItem("loseCount", loseCounter);
+// }
